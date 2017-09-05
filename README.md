598cb2af165b0650755d4bab24b68e8de539d3bf
jasperhouston <JasperHouston@dhcp-213-64.wireless.bowdoin.edu>
2017-09-05 13:36:05 -0400

    Updated hello.cpp to print x times

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # helloworld
-Write a hello world program, compile it and run it from the terminal. GO through the git cycle. 
+Write a hello world program, compile it and run it from the terminal. GO through the git cycle.
 
 compile program with the following command:
 g++ hello.cpp -o hello
 
-then run the program with ./hello
+then run the program with './hello' to print "Hello World!"
+
+alternatively, run the program with './hello x' where x is an integer > 0
+to print "Hello World!" x times
--- hello.cpp
+++ hello.cpp
@@ -3,6 +3,20 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-	cout << "Hello World!" << endl;	
+	if (argc > 2) {
+		cout << "Too many arguments. Try:" << endl;
+		cout << "To print -Hello World!- type ./hello" << endl;
+		cout << "To print -Hello World!- x times, type ./hello x, where x is an integer > 0" << endl;
+		return 0;
+	}
+	if (argc == 2) {
+		int x = stoi(argv[1]);
+		for (int i = 0; i < x; i++) {
+			cout << "Hello World!" << endl;
+		}
+		return 0;
+	}
+
+	cout << "Hello World!" << endl;
 	return 0;
 }
